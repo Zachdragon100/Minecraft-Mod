2514b096cbdd63f5f34e9a6acf74f0ba688563ca
Zachdragon100 <Zachdragon10@hotmail.com>
2014-07-14 19:48:42 -0700

    Nothing of import

--- src/main/java/com/Zachdragon100/mcraftMod/creativetab/CreativeTab.java
+++ src/main/java/com/Zachdragon100/mcraftMod/creativetab/CreativeTab.java
@@ -2,23 +2,15 @@ package com.Zachdragon100.mcraftMod.creativetab;
 
 import com.Zachdragon100.mcraftMod.init.ModItems;
 import com.Zachdragon100.mcraftMod.reference.Reference;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.util.StatCollector;
 
 public class CreativeTab {
-    public static final CreativeTabs MCMOD_TAB = new CreativeTabs(Reference.MOD_ID) {
-        @Override
-        public Item getTabIconItem() {
-            return ModItems.stickyStick;
-        }
+    public static final CreativeTabs MCMOD_TAB = new CreativeTabs(Reference.MOD_ID.toLowerCase()) {
 
         @Override
-        @SideOnly(Side.CLIENT)
-        public String getTranslatedTabLabel(){
-            return StatCollector.translateToLocal("key.categories.mcmod");
-        }
+        public Item getTabIconItem(){
+            return ModItems.stickyStick;
+        };
     };
 }
